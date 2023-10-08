232c76e1f84ab609616491efb65d5d98b25ab07a
Emmanuel Hernandez <e805177@gmail.com>
2023-10-08 12:06:52 -0600

    Feat: Icon will change after copy

--- components/tools/CopyButton.tsx
+++ components/tools/CopyButton.tsx
@@ -1,16 +1,22 @@
-import React from "react";
-import { RiFileCopyLine } from "react-icons/ri";
+import React, { useState } from "react";
+import { RiFileCopyLine, RiFileCopyFill } from "react-icons/ri";
 import toast from "react-hot-toast";
 interface Props { textToCopy: string, info?: string, className: string }
 const CopyButton = ({ textToCopy, info, className }: Props) => {
+  const [copied, setCopied] = useState(false)
   const handleCopyClick = () => {
     navigator.clipboard.writeText(textToCopy);
+    setCopied(true)
+    setTimeout(() => {
+      setCopied(false)
+    }, 1000);
     toast.success("Copied to clipboard")
   };
 
   return (
     <button onClick={handleCopyClick} className={`text-red-600 ${className}`}>
-      <RiFileCopyLine />
+      {copied  ? <RiFileCopyFill/> : <RiFileCopyLine />  }
+      
       <span>{info}</span>
     </button>
   );
